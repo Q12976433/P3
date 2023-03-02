325cf8a3790be28753695e39aa0fb755ccc27b35
Q12976433 <misxu94@gmail.com>
2023-03-02 14:08:24 +1100

    a

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Container from "@mui/material/Container";
+import TopBar from "../src/components/TopBar";
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <>
+        <TopBar />
+      </>
+    </Container>
+  );
+}
